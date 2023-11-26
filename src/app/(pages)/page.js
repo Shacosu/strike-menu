80717b9751d5f4db350b9ec304209dbd9fcb6640
gameloker <86136349+gameloker@users.noreply.github.com>
2023-11-26 00:49:42 -0300

    home page style

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -8,9 +8,9 @@ export default function Categorias() {
 			</h1>
 			<h2 className="text-center mt-8 text-2xl font-bold">CARTA</h2>
 			<div className="grid grid-cols-1 gap-4 mt-8 container mx-auto px-12">
-				<div className="bg-black border-4 border-[#ed6928] mt-3 shadow-lg text-white text-center text-2xl font-bold h-14 flex justify-center items-center rounded shadow-gray-500">SUSHI</div>
-				<div className="bg-black border-4 border-[#ed6928] mt-3 shadow-md text-white text-center text-2xl font-bold h-14 flex justify-center items-center rounded shadow-gray-500">FUSIÓN</div>
-				<div className="bg-black border-4 border-[#ed6928] mt-3 shadow-md text-white text-center text-2xl font-bold h-14 flex justify-center items-center rounded shadow-gray-500">BEBESTIBLES</div>
+				<div className="bg-black border-2 border-[#ed6928] mt-3 shadow-lg text-white text-center text-2xl font-bold h-14 flex justify-center items-center rounded shadow-gray-500">SUSHI</div>
+				<div className="bg-black border-2 border-[#ed6928] mt-3 shadow-md text-white text-center text-2xl font-bold h-14 flex justify-center items-center rounded shadow-gray-500">FUSIÓN</div>
+				<div className="bg-black border-2 border-[#ed6928] mt-3 shadow-md text-white text-center text-2xl font-bold h-14 flex justify-center items-center rounded shadow-gray-500">BEBESTIBLES</div>
 			</div>
 		</div>
 	);
